0cb5519b89b15fcbfa0061f46dac70bda82fcb96
Quinn Brittain <qgbritt@gmail.com>
2022-11-10 22:34:30 -0500

    create TicketOf functions

--- contracts/Tickets.sol
+++ contracts/Tickets.sol
@@ -6,7 +6,7 @@ contract Tickets {
     uint256 private numTickets;
     uint256 private price;
     address[] private tickets;
-    mapping (address => bool) private ticketHolders;
+    mapping (address => uint256) private ticketHolders;
 
     constructor(uint256 _numTickets, uint256 _price) {
         owner = payable(msg.sender);
@@ -26,20 +26,27 @@ contract Tickets {
         
         if (tickets[ticketID] != address(0)) {
             message = "This ticket is already taken";
-        } else if (ticketHolders[msg.sender]) {
+        } else if (ticketHolders[msg.sender] != 0) {
             message = "You already own a ticket";
         } else {
             require(msg.value >= price);
             owner.transfer(price);
             (success, data) = owner.call{value: price}("");
             tickets[ticketID] = msg.sender;
-            ticketHolders[msg.sender] = true;
+            ticketHolders[msg.sender] = ticketID;
             message = "Purchase successful";
         }
         return (message, success, data);
     }
 
-    function getTicketOf(address person) public view returns (uint256) {}
+    function getTicketOf(address person) public view returns (uint256) {
+        return ticketHolders[person];
+    }
+
+    function setTicketOf(address person, uint256 ticketID) public restricted {
+        ticketHolders[person] = ticketID;
+        tickets[ticketID] = person;
+    }
 
     function offerSwap(address partner) public {}
 
