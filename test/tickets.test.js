6167c2807819483b057ed17abf2985be669f28ff
Quinn Brittain <qgbritt@gmail.com>
2022-11-10 23:43:08 -0500

    create test cases

--- test/tickets.test.js
+++ test/tickets.test.js
@@ -11,7 +11,8 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     contract = await new web3.eth.Contract(abi)
         .deploy({
-            data: bytecode
+            data: bytecode,
+            arguments: [10, 100]
         })
         .send({
             from: accounts[0],
@@ -21,7 +22,86 @@ beforeEach(async () => {
 });
 
 describe("Tickets", () => {
-    it("Verify contract deployment", () => {
+    it("Contract deployment", () => {
         assert.ok(contract.options.address);
+    });
+    it("Buying a ticket", async () => {
+        await contract.methods.buyTicket(0).send({
+            from: accounts[0],
+            value: 100,
+            arguments: [0]
+        });
+        const tickets = await contract.methods.getTickets().call({
+            from: accounts[0],
+        });
+        assert.equal(accounts[0], ticket[0]);
+    });
+    it("Ether requirement", async () => {
+        try {
+            await contract.methods.buyTicket().send({
+                from: accounts[0],
+                value: 0,
+                arguments: [0]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+    it("Single ticket requirement", async () => {
+        try {
+            await contract.methods.buyTicket().send({
+                from: accounts[0],
+                value: 100,
+                arguments: [0]
+            });
+            await contract.methods.buyTicket().send({
+                from: accounts[0],
+                value: 100,
+                arguments: [1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+    it("Get ticket of address", async () => {
+        await contract.methods.buyTicket().send({
+            from: accounts[0],
+            value: 100,
+            arguments: [0]
+        });
+        const ticketID = await contract.methods.getTicketOf.send({
+            from: accounts[0],
+            arguments: [accounts[0]]
+        })
+        assert.equal(0, ticketID);
+    });
+    it("Swap tickets", async () => {
+        await contract.methods.buyTicket().send({
+            from: accounts[0],
+            value: 100,
+            arguments: [0]
+        });
+        await contract.methods.buyTicket().send({
+            from: accounts[1],
+            value: 100,
+            arguments: [1]
+        });
+        await contract.methods.offerSwap().send({
+            from: accounts[0],
+            value: 100,
+            arguments: [accounts[1]]
+        });
+        await contract.methods.acceptSwap().send({
+            from: accounts[1],
+            value: 100,
+            arguments: [accounts[0]]
+        });
+        const tickets = await contract.methods.getTickets().call({
+            from: accounts[0],
+        });
+        assert.equal(accounts[0], ticket[1]);
+        assert.equal(accounts[1], ticket[0]);
     });
 });
